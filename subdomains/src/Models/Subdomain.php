e9098613b5136ad909443e3202339d8f71532545
Boy132 <mail@boy132.de>
2025-12-04 16:02:15 +0100

    replace cloudflare package with manual http requests

--- subdomains/src/Models/Subdomain.php
+++ subdomains/src/Models/Subdomain.php
@@ -7,6 +7,7 @@ use Filament\Support\Contracts\HasLabel;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Http;
 
 /**
  * @property int $id
@@ -60,7 +61,6 @@ class Subdomain extends Model implements HasLabel
 
     protected function createOnCloudflare(): void
     {
-        // TODO
         /*$key = new \Cloudflare\API\Auth\APIKey(config('subdomains.email'), config('subdomains.key'));
         $adapter = new \Cloudflare\API\Adapter\Guzzle($key);
         $zones = new \Cloudflare\API\Endpoints\Zones($adapter);
@@ -69,11 +69,26 @@ class Subdomain extends Model implements HasLabel
         $zoneID = $zones->getZoneID($this->name);
 
         $dns->addRecord($zoneID, $this->record_type, $this->name, $this->server->allocation->ip, 120, false);*/
+
+        $zones = Http::cloudflare()->get('zones', [
+            'page' => 1,
+            'per_page' => 20,
+            'match' => 'all',
+            'type' => $this->record_type,
+            'name' => $this->domain->name,
+        ])->json('result');
+
+        Http::cloudflare()->post("zones/{$zones[0]->id}/dns_records", [
+            'type' => $this->record_type,
+            'name' => $this->name,
+            'content' => $this->server->allocation->ip,
+            'proxied' => false,
+            'ttl' => 120,
+        ]);
     }
 
     protected function updateOnCloudflare(): void
     {
-        // TODO
         /*$key = new \Cloudflare\API\Auth\APIKey(config('subdomains.email'), config('subdomains.key'));
         $adapter = new \Cloudflare\API\Adapter\Guzzle($key);
         $zones = new \Cloudflare\API\Endpoints\Zones($adapter);
@@ -90,11 +105,34 @@ class Subdomain extends Model implements HasLabel
             'ttl' => 120,
             'proxied' => false,
         ]);*/
+
+        $zones = Http::cloudflare()->get('zones', [
+            'page' => 1,
+            'per_page' => 20,
+            'match' => 'all',
+            'type' => $this->record_type,
+            'name' => $this->domain->name,
+        ])->json('result');
+
+        $records = Http::cloudflare()->get("zones/{$zones[0]->id}/dns_records", [
+            'page' => 1,
+            'per_page' => 20,
+            'match' => 'all',
+            'type' => $this->record_type,
+            'name' => $this->name,
+        ])->json('result');
+
+        Http::cloudflare()->post("zones/{$zones[0]->id}/dns_records/{$records[0]->id}", [
+            'type' => $this->record_type,
+            'name' => $this->name,
+            'content' => $this->server->allocation->ip,
+            'proxied' => false,
+            'ttl' => 120,
+        ]);
     }
 
     protected function deleteOnCloudflare(): void
     {
-        // TODO
         /*$key = new \Cloudflare\API\Auth\APIKey(config('subdomains.email'), config('subdomains.key'));
         $adapter = new \Cloudflare\API\Adapter\Guzzle($key);
         $zones = new \Cloudflare\API\Endpoints\Zones($adapter);
@@ -105,5 +143,23 @@ class Subdomain extends Model implements HasLabel
         $result = $dns->listRecords($zoneID, $this->record_type, $this->getLabel())->result;
 
         $dns->deleteRecord($zoneID, $result[0]->id);*/
+
+        $zones = Http::cloudflare()->get('zones', [
+            'page' => 1,
+            'per_page' => 20,
+            'match' => 'all',
+            'type' => $this->record_type,
+            'name' => $this->domain->name,
+        ])->json('result');
+
+        $records = Http::cloudflare()->get("zones/{$zones[0]->id}/dns_records", [
+            'page' => 1,
+            'per_page' => 20,
+            'match' => 'all',
+            'type' => $this->record_type,
+            'name' => $this->name,
+        ])->json('result');
+
+        Http::cloudflare()->delete("zones/{$zones[0]->id}/dns_records/{$records[0]->id}");
     }
 }
--- subdomains/src/Providers/SubdomainsPluginProvider.php
+++ subdomains/src/Providers/SubdomainsPluginProvider.php
@@ -3,6 +3,7 @@
 namespace Boy132\Subdomains\Providers;
 
 use App\Models\Role;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class SubdomainsPluginProvider extends ServiceProvider
@@ -12,5 +13,19 @@ class SubdomainsPluginProvider extends ServiceProvider
         Role::registerCustomDefaultPermissions('cloudflare_domain');
     }
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Http::macro(
+            'cloudflare',
+            fn () => Http::acceptJson()
+                ->withHeaders([
+                    'X-Auth-Email' => config('subdomains.email'),
+                    'X-Auth-Key' => config('subdomains.key'),
+                ])
+                ->timeout(5)
+                ->connectTimeout(1)
+                ->baseUrl('https://api.cloudflare.com/client/v4/')
+                ->throw()
+        );
+    }
 }
